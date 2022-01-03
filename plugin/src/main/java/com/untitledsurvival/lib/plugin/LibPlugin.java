58c846642b2699658d2ce87253224a9461e778af
ayleafs <luaq@protonmail.com>
2022-01-03 18:49:59 -0500

    initial commit

---
+++ plugin/src/main/java/com/untitledsurvival/lib/plugin/LibPlugin.java
@@ -0,0 +1,9 @@
+package com.untitledsurvival.lib.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LibPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
---
+++ src/main/java/com/untitledsurvival/lib/UntitledLib.java
@@ -0,0 +1,4 @@
+package com.untitledsurvival.lib;
+
+public class UntitledLib {
+}
