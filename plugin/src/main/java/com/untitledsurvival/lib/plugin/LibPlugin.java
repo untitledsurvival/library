e0a6225cfd089e55385619b5bec3a0b39aaec2ed
ayleafs <luaq@protonmail.com>
2022-01-04 01:32:26 -0500

    refactor: build script changes

--- plugin/src/main/java/com/untitledsurvival/lib/plugin/LibPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.untitledsurvival.lib.plugin;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class LibPlugin extends JavaPlugin {
-    @Override
-    public void onEnable() {
-    }
-}
